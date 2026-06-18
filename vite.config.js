3e4dec2aacb560d202cd205183cf63d4389f1eec
Franco Nahuel Romero <fnromero@ocasadomain.com>
2026-06-17 21:54:46 -0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  // Cambiá 'Descansos' por el nombre exacto de tu repo en GitHub si es diferente
+  base: '/Descansos/',
 })
